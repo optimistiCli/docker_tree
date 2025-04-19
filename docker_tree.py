6fb839933fae5bbed15f848ed9dc9d79d811adaf
Ish Yet13 <yet13@ema.il>
2025-04-20 01:25:08 +0200

    Added parsing of ±HH:MM timezone offsets

--- docker_tree.py
+++ docker_tree.py
@@ -196,10 +196,35 @@ class Image(object):
 
     @staticmethod
     def _parse_dt(a_str):
-        try:
-            return dt.datetime.strptime(a_str[:-4], '%Y-%m-%dT%H:%M:%S.%f')
-        except ValueError:
-            return dt.datetime.strptime(a_str[:-1], '%Y-%m-%dT%H:%M:%S')
+        for fmt,pre_pr in (
+                (
+                    '%Y-%m-%dT%H:%M:%S.%f',
+                    lambda s: \
+                        s[:-4],
+                ),
+                (
+                    '%Y-%m-%dT%H:%M:%S',
+                    lambda s: \
+                        s[:-1],
+                ),
+                (
+                    '%Y-%m-%dT%H:%M:%S.%f%z',
+                    lambda s: \
+                        re.sub(
+                            r'(\.\d{6})\d*([+\-]\d{2}):(\d{2})$',
+                            r'\1\2\3',
+                            s,
+                            ),
+                ),):
+            try:
+                return dt.datetime.strptime(
+                    a_str if pre_pr is None \
+                        else pre_pr(a_str),
+                    fmt,
+                    )
+            except ValueError:
+                continue
+        raise Exception
 
     def __init__(self, *, id=None, metadata=None):
         if (not id and not metadata) or (id and metadata):
